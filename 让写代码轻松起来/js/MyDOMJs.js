04cea36dbeebd5f9663ee73297f4cc1f81dab0c2
fanzeng <XAHfanzeng@163.com>
2017-07-18 15:40:22 +0800

    test3

---
+++ 让写代码轻松起来/js/MyDOMJs.js
@@ -0,0 +1,56 @@
+//编写addLoadEvent函数，等到页面加载完成之后再执行方法
+function addLoadEvent(fuc) {
+	var oldonload = window.onload;
+	if (typeof window.onload != "function") {
+		window.onload = fuc;
+	}else{
+		window.onload = function() {
+			oldonload();
+			fuc();
+		}
+	}
+}
+addLoadEvent(countBodyChildren);
+//编写insertAftert函数，将新元素插入到目标元素的后面
+function insertAfter(newElement, targetElement) {
+	var parent = targetElement.parentNode();
+	if (parent.lastChild == targetElement) {
+		parent.appendChild(newElement);
+	} else{
+		parent.insertBefore(newElement, targetElement.nextSibling);
+	}
+}
+//编写getHTTPObject方法，兼容所有浏览器
+function getHTTPObject() {
+	if (typeof XMLHttpRequest == "undefined") {
+		XMLHttpRequest = function() {
+			try {return new ActiveXObject("Msxml2.XMLHTTP.6.0");}
+			catch (e) {}
+			try {return new ActiveXObject("Msxml2.XMLHTTP.3.0");}
+			catch (e) {}
+			try {return new ActiveXObject("Msxml2.XMLHTTP");}
+			catch (e) {}
+			return false;
+		}
+		return new XMLHttpRequest();
+	}
+}
+//getNewContent方法，用到了addLoadEvent和getHTTPObject两个方法
+function getNewContent() {
+	var request = getHTTPObject();
+	if (request) {
+		request.open("GET", "example.text",true);
+		request.onreadystatechange = function() {
+			if (request.readyState == 4) {
+				var para = document.createElement("p");
+				var text = document.createTextNode(request.responseText);
+				para.appendChild(text);
+				document.getElementById("new").appendChild(para);
+			};
+		} 
+		request.send(null);
+	}else {
+			alert("sorry, your browser does't support XMLHTTPRequest");
+		}
+}
+addLoadEvent(getNewContent);
---
+++ 让写代码轻松起来/js/my_js.js
@@ -0,0 +1,18 @@
+//getElementsByClassName方法只在某些比较新的浏览器才支持
+//让其他浏览器也支持这一方法，写一个函数（这个方法不适用于含有多个class类名）
+function getElementsByClassName(node, classname){
+	if (node.getElementsByClassName) {
+		//使用现有的方法
+		return node.getElementsByClassName(classname);
+	} else{
+		var results = new Array();
+		var elems = node.getElementsByTagName("*");
+		for (var i = 0; i < elems.length; i++) {
+			if (elems[i].className.indexOf(classname) != -1) {
+				results[results.length] = elems[i];
+			}
+		}
+		return rusults;
+	}
+}
+
