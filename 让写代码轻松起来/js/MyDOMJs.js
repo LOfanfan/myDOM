b10b7b85ac38388b47404f06ee75544226c0ded1
fanzeng <XAHfanzeng@163.com>
2017-07-25 09:00:14 +0800

    7.25 学习

--- 让写代码轻松起来/js/MyDOMJs.js
+++ 让写代码轻松起来/js/MyDOMJs.js
@@ -54,3 +54,54 @@ function getNewContent() {
 		}
 }
 addLoadEvent(getNewContent);
+
+function displayAbbreviations() {
+	
+	if (!document.getElementsByTagName) return false;
+	if (!document.createElement) return false;
+	if (!document.createTextNode) return false;
+	//取得缩略词
+	var abbreviations = document.getElementsByTagName("abbr");
+	//判断是否存在这个元素，没有的话退出来
+	if (abbreviations.length < 1) return false;
+	var defs = new Array();
+	//遍历缩略词
+	for (var i = 0; i < abbreviations.length; i++) {
+		var current_abbr = abbreviations[i];
+		if (current_abbr.length < 1) return false;
+		var definition = current_abbr.getAttribute("title");
+		var key = abbreviations[i].lastChild.nodeValue;
+		defs[key] = definition;
+	}
+	//创建自定义列表
+		var dlist = document.createElement("dt");
+	for (key in defs) {
+		var definition = defs[key];
+		//创建定义标题
+		var dtitle = document.createElement("dl");
+		var dtitle_text = document.createTextNode(key);
+		dtitle.appendChild(dtitle_text);
+		var ddesc = document.createElement("dd");
+		var ddesc_text = document.createTextNode(definition);
+		ddesc.appendChild(ddesc_text);
+		//把他们添加到定义列表
+		dlist.appendChild(dtitle);
+		dlist.appendChild(ddesc);
+	}
+	//创建标题
+	var header = document.createElement("h2");
+	var header_text = document.createTextNode("Abbreviations");
+	header.appendChild(header_text);
+	//将标题添加到页面主体
+	document.body.appendChild(header);
+	//把定义列表添加到页面主体
+	document.body.appendChild(dlist);
+}
+document.getElementsByClassName()
+function displayCitations() {
+	var quotes = document.getElementsByTagName("blockquote");
+	for (var i = 0; i < quotesgth; i++) {
+		if (!quotes[i].getAttribute("cite")) continue;
+		var url = quotes[i].getAttribute("cite");
+	}
+}
